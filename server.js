7639c01b5d338e6757f08cd26cae4fcf680652ad
1Mathias <matimen9@yahoo.com>
2021-10-03 02:34:39 +0300

    Heroku Devops

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/gebeya-online'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/gebeya-online/'}),
 );
 
 // Start the app by listening on the default Heroku port
